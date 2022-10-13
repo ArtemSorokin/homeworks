5c8cd9dc7e3ac77d140adf20a46fffae25ce3e03
ArtemSorokin <artemsorokin4395@gmail.com>
2022-10-13 13:47:47 +0300

    h5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {PATH} from "./Pages";
+import s from './Header.module.css'
+
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.head}>
+
+            <div className={s.main}>
+                <NavLink to={PATH.PRE_JUNIOR} className={s.link}>PreJunior</NavLink>
+            </div>
+            <div className={s.main}>
+                <NavLink to={PATH.JUNIOR} className={s.link}>Junior</NavLink>
+            </div>
+            <div className={s.main}>
+                <NavLink to={PATH.MIDDLE} className={s.link}>Middle</NavLink>
+            </div>
+
+            <span className={s.block}> Levels </span>
+
 
         </div>
     )
